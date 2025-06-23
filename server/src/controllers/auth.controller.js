f38bea9b7c7dfd2925e09a66338cb02f2617a66e
devdesignerprakash <devdesignerprakash@gmail.com>
2025-06-23 23:22:40 +0545

    auth api

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+import User from "../models/user.model.js"
+import { generateToken } from "../utils/token.js"
+import bcrypt from 'bcryptjs'
+export const signUp=async(req,res)=>{
+    try{
+        const {fullName,email, address, gender,userName, password}=req.body
+        const newUser= await User.create({
+            fullName,
+            email,
+            address,
+            gender,
+            userName,
+            password
+        })
+       const token= generateToken(newUser._id)
+       res.status(201).json({
+        msg:"user created successfully",
+        newUser,
+        token
+       })
+    }catch(error){
+        res.status(500).json({ error: error.message });
+
+    }
+}
+export const login=async(req,res)=>{
+    try{
+        const {userName,email, password}=req.body
+        if(!userName &&!email){
+            return res.status(400).json({msg:'User Name or Email is required'})
+
+        }
+        const existUser= await User.findOne({
+            $or:[{userName:userName},{email:email}]
+        })
+        if(!existUser){
+            return res.status(400).json({msg:"user not found"})
+        }
+        const isPasswordMatch= bcrypt.compareSync(password,existUser.password)
+        if(!isPasswordMatch){
+            return res.status(401).json({msg:"Invlaid Crenditals"})
+        }
+        const token=generateToken(existUser._id)
+        res.status(200).json({
+            msg:"user logged in successfully",
+            token
+        })  
+    }
+    catch(error){
+        res.status(500).json({error:error.message})
+    }
+
+}
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,6 @@
 import express from 'express'
-import {login, signUp } from '../controllers/user.controller.js'
+import {login, signUp } from '../controllers/auth.controller.js'
 const userRouter= express.Router()
 userRouter.post('/signup',signUp)
 userRouter.post('/login',login)
-
-
 export default userRouter
