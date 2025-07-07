c1ef2f765c44aa8ca8fd220bc01692a5a7feb12c
devdesignerprakash <devdesignerprakash@gmail.com>
2025-07-07 00:51:38 -0700

    viewcontent and published or unpublished functionality on progress

--- client/src/components/adminComponents/ViewPostContent.jsx
+++ client/src/components/adminComponents/ViewPostContent.jsx
@@ -4,7 +4,7 @@ import {useLocation} from 'react-router-dom'
 const ViewPostContent = () => {
   const location = useLocation();
   const post = location.state?.post || {}; // Get post from location state or default to empty object
-    console.log("********************************",post)
+    console.log("********************************",post.content)
   if (!post || Object.keys(post).length === 0) {
     return <div className='text-red-500'>पोस्ट फेला परेन।</div>;
   }
@@ -16,6 +16,12 @@ const ViewPostContent = () => {
         <div className='flex justify-between items-center'>
             <span className='text-sm text-gray-500'>लेखक: {post.author}</span>
             <span className='text-sm text-gray-500'>मिति: {new Date(post.createdAt).toLocaleDateString()}</span>
+            <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
+                {post.status === 'published' ? 'प्रकाशित' : 'प्रकाशन गर्नुहोस्'}
+            </button>
+            <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600'>
+                मेटाउनुहोस्                                                                    
+              </button>
         </div>
     </div>
     </>
